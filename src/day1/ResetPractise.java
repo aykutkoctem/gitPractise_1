ebbce29e0b597181ee7f20a48266043042524ef0
aykutkoctem <aykutkoctem@gmail.com>
2021-07-28 16:18:53 -0700

    test case all done

--- src/day1/ResetPractise.java
+++ src/day1/ResetPractise.java
@@ -4,4 +4,8 @@ public class ResetPractise {
     // part 1 is done
     // part 2 is done
     // part 3 is done
+    // part 4 is done
+    // part 5 is done
+    // mixed|  Soft Did part 4 part 5 and I am done in one commit
+
 }
