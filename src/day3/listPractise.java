e7c080abf465bae20a484d48e4680eaabb14d341
aykutkoctem <aykutkoctem@gmail.com>
2021-07-29 11:33:03 -0700

    creating new arraylist

--- src/day3/listPractise.java
+++ src/day3/listPractise.java
@@ -1,5 +1,12 @@
 package day3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class listPractise {
+
+    public static void main(String[] args) {
+        List<String> lst=new ArrayList<>();
+    }
 
 }
